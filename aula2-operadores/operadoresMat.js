f2beb60305a07a5ab8c8a843d4fcfb5890a14acf
AlanFaraj83 <alanfaraj@outlook.com>
2024-03-26 22:15:14 -0300

    Aula de hoje 260324

---
+++ aula2-operadores/operadoresMat.js
@@ -0,0 +1,18 @@
+let numero1 = 6;
+let numero2 = 2;
+console.log("");
+console.log("--------------------");
+console.log(`Numero1: ${numero1}`);
+console.log(`Numero2: ${numero2}`);
+console.log("--------------------");
+console.log("");
+
+//Operadores aritméticos
+
+// Soma
+let soma = numero1 + numero2;
+console.log("A soma é:" +  soma);
+console.log(`A soma é: ${soma}`);
+
+const CPF = "123.345.678-00";
+console.log(CPF);
